23a1a92bf1cc0f8e15c5f109001ed2ac30c4887d
Prince John <prince.apple.john@gmail.com>
2019-01-17 00:38:12 +0530

    Adding test sketch

---
+++ DataLogger_test/DataLogger_test.ino
@@ -0,0 +1,165 @@
+
+/*
+This program writes at a constant interval not at state change. 
+
+*/
+
+
+#include <SPI.h>
+#include <SD.h>
+#include <Wire.h>
+#include "RTClib.h"
+
+// A simple data logger for the Arduino analog pins
+
+// how many milliseconds between grabbing data and logging it. 1000 ms is once a second
+#define LOG_INTERVAL  1000 // mills between entries (reduce to take more/faster data)
+
+// how many milliseconds before writing the logged data permanently to disk
+// set it to the LOG_INTERVAL to write each time (safest)
+// set it to 10*LOG_INTERVAL to write all data every 10 datareads, you could lose up to 
+// the last 10 reads if power is lost but it uses less power and is much faster!
+#define SYNC_INTERVAL 1000 // mills between calls to flush() - to write data to the card
+uint32_t syncTime = 0; // time of last sync()
+
+
+// the digital pins that connect to the LEDs
+#define redLEDpin 2
+#define greenLEDpin 3
+
+
+#define tempPin 0
+
+RTC_PCF8523 RTC; // define the Real Time Clock object
+
+// for the data logging shield, we use digital pin 10 for the SD cs line
+const int chipSelect = 10;
+
+// the logging file
+File logfile;
+
+void error(char *str)
+{
+  Serial.print("error: ");
+  Serial.println(str);
+  
+  // red LED indicates error
+  digitalWrite(redLEDpin, HIGH);
+
+  while(1);
+}
+
+void setup(void)
+{
+  Serial.begin(9600);
+  Serial.println();
+  
+  // use debugging LEDs
+  pinMode(redLEDpin, OUTPUT);
+  pinMode(greenLEDpin, OUTPUT);
+
+  pinMode(tempPin, INPUT);
+
+
+  // initialize the SD card
+  Serial.print("Initializing SD card...");
+  // make sure that the default chip select pin is set to
+  // output, even if you don't use it:
+  pinMode(10, OUTPUT);
+  
+  // see if the card is present and can be initialized:
+  if (!SD.begin(chipSelect)) {
+    error("Card failed, or not present");
+  }
+  Serial.println("card initialized.");
+  
+  // create a new file
+  char filename[] = "LOGGER00.CSV";
+  for (uint8_t i = 0; i < 100; i++) {
+    filename[6] = i/10 + '0';
+    filename[7] = i%10 + '0';
+    if (! SD.exists(filename)) {
+      // only open a new file if it doesn't exist
+      logfile = SD.open(filename, FILE_WRITE); 
+      break;  // leave the loop!
+    }
+  }
+  
+  if (! logfile) {
+    error("couldnt create file");
+  }
+  
+  Serial.print("Logging to: ");
+  Serial.println(filename);
+
+  // connect to RTC
+  Wire.begin();  
+  if (!RTC.begin()) {
+    logfile.println("RTC failed");
+
+  
+
+  logfile.println("datetime,temp");    
+
+ 
+}
+}
+
+void loop(void)
+{
+  DateTime now;
+
+  // delay for the amount of time we want between readings
+  delay((LOG_INTERVAL -1) - (millis() % LOG_INTERVAL));
+  
+  digitalWrite(greenLEDpin, HIGH);
+  
+  // log milliseconds since starting
+ 
+
+  // fetch the time
+  now = RTC.now();
+  // log time
+  logfile.print(now.unixtime()); // seconds since 1/1/1970
+  logfile.print(", ");
+  logfile.print('"');
+  logfile.print(now.year(), DEC);
+  logfile.print("/");
+  logfile.print(now.month(), DEC);
+  logfile.print("/");
+  logfile.print(now.day(), DEC);
+  logfile.print(" ");
+  logfile.print(now.hour(), DEC);
+  logfile.print(":");
+  logfile.print(now.minute(), DEC);
+  logfile.print(":");
+  logfile.print(now.second(), DEC);
+  logfile.print('"');
+
+
+  
+  int tempReading = analogRead(tempPin);    
+  
+  // converting that reading to voltage, for 3.3v arduino use 3.3
+  float voltage = tempReading * 3.3 / 1024;  
+  float temperature = (voltage - 0.5) * 100 ;
+ 
+
+  logfile.print(", ");    
+  logfile.print(temperature);
+
+  logfile.println();//change line
+ 
+  digitalWrite(greenLEDpin, LOW);
+
+  // Now we write data to disk! Don't sync too often - requires 2048 bytes of I/O to SD card
+  // which uses a bunch of power and takes time
+  if ((millis() - syncTime) < SYNC_INTERVAL) return;
+  syncTime = millis();
+  
+  // blink LED to show we are syncing data to the card & updating FAT!
+  digitalWrite(redLEDpin, HIGH);
+  logfile.flush();
+  digitalWrite(redLEDpin, LOW);
+}
+  
